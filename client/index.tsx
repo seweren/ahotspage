c03544680bfe9d0607831fb62ef391782cbdbec1
seweren <seweren@gmail.com>
2017-04-12 20:59:37 +0200

    hello world

--- client/index.tsx
+++ client/index.tsx
@@ -1,2 +1,4 @@
-console.log("cicca");
-document.body.innerHTML = "<div>cicca</div>";
+import * as React from "react";
+import { render } from "react-dom";
+
+render(<div>cicca</div>, document.getElementById("root"));
--- server/server.ts
+++ server/server.ts
@@ -7,9 +7,7 @@ const logger = new console.Console(process.stdout);
 const app = express();
 
 app.use(morgan("combined"));
-app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
-});
+app.use(express.static(path.join(__dirname, "..", "client")));
 
 const server = app.listen(8000, "localhost", () => {
   logger.info("Listening on http://localhost:" + server.address().port);
